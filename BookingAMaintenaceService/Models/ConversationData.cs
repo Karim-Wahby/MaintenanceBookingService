c6165f7c6e7372abfe4d13ea757757efc0544ece
Karim Wahby <a-kawahb>
2019-04-16 02:02:17 +0200

    some model classes to represent some of the user data.

---
+++ BookingAMaintenaceService/Models/ConversationData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingAMaintenaceService.Models
+{
+    public class ConversationData
+    {
+        public ConversationPhases ConversationState { get; set; } = ConversationPhases.GreetingTheUser;
+        public int Counter { get; set; } = 0;
+    }
+}
---
+++ BookingAMaintenaceService/Models/UserData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingAMaintenaceService.Models
+{
+    public class UserData
+    {
+        public string HomeAddress { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
